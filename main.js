15d0bf852db7593a027baa15ff0fef4c39b561de
rk4500 <87379158+rk4500@users.noreply.github.com>
2025-02-18 16:59:19 +0530

    built

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-// Function to Load Pages Dynamically
-// function loadPage(page, specfic=null) {
-//     fetch(page)
-//         .then(response => response.text())
-//         .then(html => {
-//             document.getElementById("page-content").innerHTML = html;
-//         })
-//         .catch(error => console.error("Error loading page:", error));
-// }
-let lastAnim;
+let currentAction = 'home';
+let lastAction;
 
-function loadPage(page, specfic=null) {
+function loadPage(page, backflag=false) {
+
+    // Saving Anim
+    lastAction = currentAction;
+    currentAction = page;
+
     const contentDiv = document.getElementById("page-content");
-    lastAnim = specfic ? specfic: 'Appear';
     // Start fade out
     contentDiv.classList.add("fade-out");
 
     setTimeout(() => {
-        fetch(page)
-            .then(response => response.text())
+        fetch('pages/${page}.html')
+            .then(response => {return response.text();})
             .then(html => {
                 contentDiv.innerHTML = html;
                 contentDiv.classList.remove("fade-out");
@@ -28,13 +24,29 @@ function loadPage(page, specfic=null) {
             })
             .catch(error => console.error("Error loading page:", error));
     }, 500);
+    transition(page);
 }
 
 // Handle browser back/forward navigation
 window.addEventListener("popstate", (event) => {
     if (event.state && event.state.page) {
-        console.log(lastAnim);
-        loadPage(event.state.page);
-        transition(lastAnim);
+        loadPage(event.state.page, lastAction, true);
+    }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const currentPage = urlParams.get("redirect") || window.location.pathname.split('/').at(-1) ||"home";  // Default to home
+    loadPage(currentPage, false); 
+    setTimeout(() => {
+        loadPage(currentPage, false);  
+    }, 100);
+});
+
+window.addEventListener("click", (event) => {
+    const target = event.target.closest("a");
+    if(target && target.getAttribute("href") && !target.getAttribute("href").startsWith("https")) {
+        event.preventDefault();
+        loadPage(target.getAttribute("href"), target.getAttribute("id"));
     }
 });
